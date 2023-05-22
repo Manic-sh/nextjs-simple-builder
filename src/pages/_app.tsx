b2ca842cb3b4d11db266d4c43ccf93d4f7aa1e31
Manic-sh <mash.pro666@gmail.com>
2023-05-22 18:41:52 +0530

    initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,108 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Builder } from '@builder.io/react'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
+
+
+const VimeoPlayerTracking = () => (
+  <script
+    dangerouslySetInnerHTML={{
+      __html: `
+<script src="https://player.vimeo.com/api/player.js"></script>
+<script>
+(function() {
+   var config = {
+      webPagePseudoRoot: "/vimeovideotracking",
+      eventPaths: {
+         play: "play",
+         pause: "pause",
+         ended: "finish"
+      }
+   };
+
+   /* --- NO NEED TO TOUCH ANYTHING BELOW THIS LINE! --- */
+
+   var arrayify = getSelection.call.bind([].slice);
+
+   var vimeoStor = "IFRAME[src^='https://player.vimeo.com/video/']",
+      vimeoDOMObjects = document.querySelectorAll(vimeoStor);
+
+   function normalize(eventData) {
+      var transformOpts = {
+         scale: {
+            percent: 100
+         },
+         round: {
+            seconds: 0
+         },
+         SCALE_NO_SCALE: 1,
+         ROUND_2_DIGITS: 2
+      };
+      normData = {};
+
+      Object.keys(eventData).forEach(function(key) {
+         var scaleFactor =
+               transformOpts.scale[key] !== undefined
+                  ? transformOpts.scale[key]
+                  : transformOpts.SCALE_NO_SCALE,
+            roundFactor =
+               transformOpts.round[key] !== undefined
+                  ? transformOpts.round[key]
+                  : transformOpts.ROUND_2_DIGITS,
+            prePadding = Math.round(
+               eventData[key].toFixed(roundFactor) * scaleFactor
+            ),
+            padFactor = prePadding < 10 ? "0" : ""; // left-pad single digits for easier Marketo matching
+
+         normData[key] = padFactor + prePadding;
+      });
+
+      return normData;
+   }
+
+   arrayify(vimeoDOMObjects)
+      .map(function(vimeoDOMObject) {
+         return new Vimeo.Player(vimeoDOMObject);
+      })
+      .forEach(function(vimeoPlayer) {
+         vimeoPlayer.getVideoId().then(function(videoId) {
+            ["play", "pause", "ended"].forEach(function(eventType) {
+               vimeoPlayer.on(eventType, function(eventData) {
+                  var statsNormalized = normalize(eventData);
+
+                  var eventAsPath = [
+                     config.webPagePseudoRoot,
+                     videoId,
+                     config.eventPaths[eventType]
+                  ].join("/");
+
+                  var statsAsSearch = ["seconds", "percent"]
+                     .map(function(stat) {
+                        return [stat, statsNormalized[stat]].join("=");
+                     })
+                     .join("&");
+
+                  var munchkinPayload = {
+                     url: eventAsPath,
+                     params: statsAsSearch
+                  };
+
+                  Munchkin.munchkinFunction("visitWebPage", munchkinPayload);
+               });
+            });
+         });
+      });
+})();
+</script>
+`,
+    }}
+  />
+);
+
+Builder.registerComponent(VimeoPlayerTracking, {
+  name: "VimeoPlayerTracking",
+
+});
